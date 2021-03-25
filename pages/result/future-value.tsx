1c6651817510e35adba300b86956a6d18102219f
Bruno Haetinger <brunohaetinger@gmail.com>
2021-03-24 22:07:06 -0300

    Create loading component

--- pages/result/future-value.tsx
+++ pages/result/future-value.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Loading } from "../../src/components/Loading";
 import { useFutureValueResult } from "../../src/hooks/useFutureValueResult";
 
-
 const FutureValueResultPage = () => {
-  const {query} = useRouter();
-  const {initialAmount, interestRate, periods } = query;
+  const { query } = useRouter();
+  const { initialAmount, interestRate, periods } = query;
   const result = useFutureValueResult(initialAmount, interestRate, periods);
 
   return (
     <>
-      { result === null ? <h2>Loading...</h2> : <h1>Future Value Result Page : {result}</h1> }
+      {result === null ? (
+        <Loading />
+      ) : (
+        <h1>Future Value Result Page : {result}</h1>
+      )}
       <Link href="/">
         <a>Go Home</a>
       </Link>
